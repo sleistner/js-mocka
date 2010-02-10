5a1c609989bc7c0e404abb1e96c92c4096dec61f
Steffen Leistner <sleistner@Mapplayer-MacBook-Pro.local>
2010-02-10 19:05:56 +0100

    format

--- src/expectation.js
+++ src/expectation.js
@@ -7,16 +7,16 @@
 JSMocka.Expectation = function(object, method) {
 
 	var original = null,
-		parametersMatcher = new JSMocka.Matcher.ParametersMatcher([new JSMocka.Matcher.AnyParametersMatcher()]),
-		cardinalityMatcher = new JSMocka.Matcher.ExactCardinalityMatcher(1),
-		correctCardinality,
-		correctParameters = true,
-		timesCalled = 0,
-		callArguments = [],
-		returnValues = [],
-		negated = false,
-		evaluable = true,
-		patched = false;
+	parametersMatcher = new JSMocka.Matcher.ParametersMatcher([new JSMocka.Matcher.AnyParametersMatcher()]),
+	cardinalityMatcher = new JSMocka.Matcher.ExactCardinalityMatcher(1),
+	correctCardinality,
+	correctParameters = true,
+	timesCalled = 0,
+	callArguments = [],
+	returnValues = [],
+	negated = false,
+	evaluable = true,
+	patched = false;
 
 	/**
 	* @inner
@@ -33,11 +33,11 @@ JSMocka.Expectation = function(object, method) {
 	}
 
 	/**
-   	* @inner
-   	* Moves the original expected method away and replaces it with a method
-   	* showing the expected behaviour.
-   	* This method is automatically called.
-   	*/
+	* @inner
+	* Moves the original expected method away and replaces it with a method
+	* showing the expected behaviour.
+	* This method is automatically called.
+	*/
 	this.apply = function() {
 		if (!patched) {
 			original = object[method];
@@ -47,31 +47,31 @@ JSMocka.Expectation = function(object, method) {
 	};
 
 	/**
-   	* @inner
-   	* Restores the original expected method. This method is automatically called
-   	* on verify.
-   	*/
-    this.restore = function() {
-        if (patched) {
-            object[method] = original;
-            original = null;
-        }
-    };
+	* @inner
+	* Restores the original expected method. This method is automatically called
+	* on verify.
+	*/
+	this.restore = function() {
+		if (patched) {
+			object[method] = original;
+			original = null;
+		}
+	};
 
-  	/**
-   	* @inner
-   	* Evaluates if the expectations have been met.
-   	*/
-    this.evaluate = function() {
-        correctCardinality = cardinalityMatcher.match(timesCalled);
-        var satisfied = correctParameters && correctCardinality;
-        return negated ? !satisfied : satisfied;
-    };
+	/**
+	* @inner
+	* Evaluates if the expectations have been met.
+	*/
+	this.evaluate = function() {
+		correctCardinality = cardinalityMatcher.match(timesCalled);
+		var satisfied = correctParameters && correctCardinality;
+		return negated ? !satisfied : satisfied;
+	};
 
-  	/**
-   	* @inner
-   	* Provides a short description of the expectation.
-   	*/
+	/**
+	* @inner
+	* Provides a short description of the expectation.
+	*/
 	this.toString = function() {
 		var string = (negated ? "NOT " : "") + "Call of method '" + method;
 		string += "' with " + parametersMatcher.toString();
@@ -83,11 +83,11 @@ JSMocka.Expectation = function(object, method) {
 		return string;
 	};
 
-  	/**
-   	* @inner
-   	* Provides a detailled description of the expectation, including which
-   	* expectations have been satisfied, and which have not.
-   	*/
+	/**
+	* @inner
+	* Provides a detailled description of the expectation, including which
+	* expectations have been satisfied, and which have not.
+	*/
 	this.details = function() {
 		var string = 'Parameters were' + (correctParameters ? '' : ' NOT') + ' correctly matched. ';
 		string += 'Expected number of calls was' + (correctCardinality ? '' : ' NOT') + ' matched. ';
@@ -119,164 +119,164 @@ JSMocka.Expectation = function(object, method) {
 	* @returns {JSMocka.Expectation} the object itself for chaining
 	* @see JSMocka.Matcher
 	*/
-    this.withArgs = function(args) {
-        parametersMatcher = new JSMocka.Matcher.ParametersMatcher(arguments);
-        return this;
-    };
+	this.withArgs = function(args) {
+		parametersMatcher = new JSMocka.Matcher.ParametersMatcher(arguments);
+		return this;
+	};
 
-  	/**
-   	* Syntactic sugar for complex expectations. Actually does nothing.
+	/**
+	* Syntactic sugar for complex expectations. Actually does nothing.
 	* @returns {JSMocka.Expectation} the object itself for chaining
-   	*/
-    this.then = function() {
-        return this;
-    };
+	*/
+	this.then = function() {
+		return this;
+	};
 
-  	/**
-   	* Modifies the expectation so that the expected method must be called the
-   	* specified amount of times.
-   	* @example
-   	* JSMocka.create({}).expects('foo').times(3);
+	/**
+	* Modifies the expectation so that the expected method must be called the
+	* specified amount of times.
+	* @example
+	* JSMocka.create({}).expects('foo').times(3);
 	* @param card {number} the expected number of calls
 	* @returns {JSMocka.Expectation} the object itself for chaining
-   	*/
-    this.times = function(card) {
-        cardinalityMatcher = new JSMocka.Matcher.ExactCardinalityMatcher(card);
-        return this;
-    };
+	*/
+	this.times = function(card) {
+		cardinalityMatcher = new JSMocka.Matcher.ExactCardinalityMatcher(card);
+		return this;
+	};
 
-  	/**
-   	* Modifies the expectation so that the expected method may be called any
-   	* amount of times.
+	/**
+	* Modifies the expectation so that the expected method may be called any
+	* amount of times.
 	* @returns {JSMocka.Expectation} the object itself for chaining
-   	*/
-    this.anyTime = function() {
-        cardinalityMatcher = new JSMocka.Matcher.AnyCardinalityMatcher();
-        return this;
-    };
+	*/
+	this.anyTime = function() {
+		cardinalityMatcher = new JSMocka.Matcher.AnyCardinalityMatcher();
+		return this;
+	};
 
-  	/**
-   	* Modifies the expectation so that the expected method must not be called at
-   	* all.
+	/**
+	* Modifies the expectation so that the expected method must not be called at
+	* all.
 	* @returns {JSMocka.Expectation} the object itself for chaining
-   	*/
-    this.never = function() {
-        return this.times(0);
-    };
+	*/
+	this.never = function() {
+		return this.times(0);
+	};
 
-  	/**
-   	* Modifies the expectation so that the expected method must be called exactly
-   	* once. This is the default setting.
+	/**
+	* Modifies the expectation so that the expected method must be called exactly
+	* once. This is the default setting.
 	* @returns {JSMocka.Expectation} the object itself for chaining
-   	*/
-    this.once = function() {
-        return this.times(1);
-    };
+	*/
+	this.once = function() {
+		return this.times(1);
+	};
 
- 	/**
-   	* Modifies the expectation so that the expected method must be called exactly
-   	* twice.
+	/**
+	* Modifies the expectation so that the expected method must be called exactly
+	* twice.
 	* @returns {JSMocka.Expectation} the object itself for chaining
-   	*/
-    this.twice = function() {
-        return this.times(2);
-    };
+	*/
+	this.twice = function() {
+		return this.times(2);
+	};
 
-  	/**
-   	* Modifies the expectation so that the expected method must be called at
-   	* least the specified amount of times.
+	/**
+	* Modifies the expectation so that the expected method must be called at
+	* least the specified amount of times.
 	* @param times {number} the min number number of expected calls
 	* @returns {JSMocka.Expectation} the object itself for chaining
-   	*/
-    this.atLeast = function(times) {
-        cardinalityMatcher = new JSMocka.Matcher.MinimumCardinalityMatcher(times);
-        return this;
-    };
+	*/
+	this.atLeast = function(times) {
+		cardinalityMatcher = new JSMocka.Matcher.MinimumCardinalityMatcher(times);
+		return this;
+	};
 
-  	/**
-   	* Modifies the expectation so that the expected method must be called at
-   	* most the specified amount of times.
+	/**
+	* Modifies the expectation so that the expected method must be called at
+	* most the specified amount of times.
 	* @param times {number} the max number number of expected calls
 	* @returns {JSMocka.Expectation} the object itself for chaining
-   	*/
-    this.atMost = function(times) {
-        cardinalityMatcher = new JSMocka.Matcher.MaximumCardinalityMatcher(times);
-        return this;
-    };
+	*/
+	this.atMost = function(times) {
+		cardinalityMatcher = new JSMocka.Matcher.MaximumCardinalityMatcher(times);
+		return this;
+	};
 
-  	/**
-   	* Modifies the expectation so that the expected method must be called any
-   	* amount of times in the interval specified by from and to.
+	/**
+	* Modifies the expectation so that the expected method must be called any
+	* amount of times in the interval specified by from and to.
 	* @param from {number} the min number number of expected calls
 	* @param to {number} the max number number of expected calls
 	* @returns {JSMocka.Expectation} the object itself for chaining
-   	*/
-    this.between = function(from, to) {
-        cardinalityMatcher = new JSMocka.Matcher.RangeCardinalityMatcher(from, to);
-        return this;
-    };
+	*/
+	this.between = function(from, to) {
+		cardinalityMatcher = new JSMocka.Matcher.RangeCardinalityMatcher(from, to);
+		return this;
+	};
 
-  	/**
-   	* Negates the expectation, such that it the evaluation fails if all specified
-   	* criteria are met.
-   	* @example
-   	* // In the following example, no instance of class Foo may be created.
-   	*
-   	* var Foo = new function() {};
-   	* JSMocka.create(Foo).anyInstance().not();
+	/**
+	* Negates the expectation, such that it the evaluation fails if all specified
+	* criteria are met.
+	* @example
+	* // In the following example, no instance of class Foo may be created.
+	*
+	* var Foo = new function() {};
+	* JSMocka.create(Foo).anyInstance().not();
 	* @returns {JSMocka.Expectation} the object itself for chaining
-   	*/
-    this.not = function() {
-        negated = true;
-        return this;
-    };
+	*/
+	this.not = function() {
+		negated = true;
+		return this;
+	};
 
-  	/**
-   	* Modifies the expectation so that when the expected method is called, it
-   	* returns the specified value.
-   	*
-   	* This method may be called with multiple arguments, or multiple times. In
-   	* this case, the values are returned in turn on consecutive calls to the
-   	* method.
-   	* @example
+	/**
+	* Modifies the expectation so that when the expected method is called, it
+	* returns the specified value.
+	*
+	* This method may be called with multiple arguments, or multiple times. In
+	* this case, the values are returned in turn on consecutive calls to the
+	* method.
+	* @example
 	* var o = {};
-   	* m = JSMocka.create(o);
-   	* m.stubs('foo').returns(10, 12);
-   	* m.stubs('bar').returns(14).then().returns(16);
+	* m = JSMocka.create(o);
+	* m.stubs('foo').returns(10, 12);
+	* m.stubs('bar').returns(14).then().returns(16);
 	*
 	* @returns {JSMocka.Expectation} the object itself for chaining
-   	*/
-    this.returns = function() {
-        returnValues = returnValues.concat(JSMocka.toArray(arguments));
-        return this;
-    };
+	*/
+	this.returns = function() {
+		returnValues = returnValues.concat(JSMocka.toArray(arguments));
+		return this;
+	};
 
 	/**
 	* @inner
 	*/
-    this.isEvaluable = function() {
-        return evaluable;
-    };
+	this.isEvaluable = function() {
+		return evaluable;
+	};
 
 	/**
 	* @inner
 	*/
-    this.setEvaluable = function(shouldEvaluate) {
-        evaluable = shouldEvaluate;
-        return this;
-    };
+	this.setEvaluable = function(shouldEvaluate) {
+		evaluable = shouldEvaluate;
+		return this;
+	};
 };
 
 /** @inner */
 JSMocka.Expectation.onStubbing = function(object, method) {
-    if (JSMocka.Configuration.shouldPrevent('StubbingNonExistentMethod', object)) {
-        if (!(method in object)) {
-            throw new Error('JSMocka::Error.Stubbing non existent method: ' + method);
-        }
-    }
-    if (JSMocka.Configuration.shouldPrevent('StubbingNonPublicMethod', object)) {
-        if ((/^_/).test(method)) {
-            throw new Error('JSMocka::Expectation.Stubbing non public method: ' + method);
-        }
-    }
+	if (JSMocka.Configuration.shouldPrevent('StubbingNonExistentMethod', object)) {
+		if (!(method in object)) {
+			throw new Error('JSMocka::Expectation.Stubbing non existent method: ' + method);
+		}
+	}
+	if (JSMocka.Configuration.shouldPrevent('StubbingNonPublicMethod', object)) {
+		if ((/^_/).test(method)) {
+			throw new Error('JSMocka::Expectation.Stubbing non public method: ' + method);
+		}
+	}
 };
